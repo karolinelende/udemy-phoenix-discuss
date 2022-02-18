48c2b16ee84d4be8e65efcee0149e7186f35781a
Karoline Lende <karoline.lende@multiverse.io>
2022-02-18 14:14:21 +0000

    complete application to final course video

--- lib/discuss/accounts/user.ex
+++ lib/discuss/accounts/user.ex
@@ -1,12 +1,15 @@
 defmodule Discuss.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
-  # use DiscussWeb, :model
+
+  @derive {Jason.Encoder, only: [:email]}
 
   schema "users" do
     field :email, :string
     field :provider, :string
     field :token, :string
+    has_many :topics, Discuss.Discussions.Topic
+    has_many :comments, Discuss.Discussions.Comment
 
     timestamps()
   end
